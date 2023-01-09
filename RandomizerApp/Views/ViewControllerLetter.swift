386c1315e7509fcced655927f72f6f201c775f47
Manohin <111537419+Manohin@users.noreply.github.com>
2023-01-09 22:27:19 +0300

    Bug fixed

--- RandomizerApp/Views/ViewControllerLetter.swift
+++ RandomizerApp/Views/ViewControllerLetter.swift
@@ -20,7 +20,13 @@ class ViewControllerLetter: UIViewController {
     @IBAction func resultButton(_ sender: UIButton) {
         resultLabel.isHidden = false
         if rusEngSC.selectedSegmentIndex == 0 {
-            let rusAlphabet:[Character] = ["А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"]
+            let rusAlphabet:[Character] = [
+                "А","Б","В","Г","Д","Е","Ё",
+                "Ж","З","И","Й","К","Л","М",
+                "Н","О","П","Р","С","Т","У",
+                "Ф","Х","Ц","Ч","Ш","Щ","Ъ",
+                "Ы","Ь","Э","Ю","Я"
+            ]
             
             if let result = rusAlphabet.randomElement() {
                 resultLabel.text = (String(result))
@@ -29,28 +35,21 @@ class ViewControllerLetter: UIViewController {
             }
         }
         if rusEngSC.selectedSegmentIndex == 1 {
-            let engAlphabet:[Character] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-            
+            let engAlphabet:[Character] = [
+                "A","B","C","D","E","F","G",
+                "H","I","J","K","L","M","O",
+                "P","Q","R","S","T","U","V",
+                "W","X","Y","Z"
+            ]
             if let result = engAlphabet.randomElement() {
                 resultLabel.text = (String(result))
             } else {
                 return
             }
-           
         }
     }
     @IBAction func closeButton(_ sender: UIButton) {
         
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
